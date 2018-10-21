0af0415dc152b78975b9c190bce572238152cd13
amrwc <42574496+amrwc@users.noreply.github.com>
2018-10-21 22:38:40 +0100

    Fix tests
    - replace exit labels in the menu file,
    - remove redundant comments.
    
    The 'end_tests' label used in the menu file skipped tests, thus causing issues with line drawing which the tests normally prevented.

--- stage1/graphics_line_tests.asm
+++ stage1/graphics_line_tests.asm
@@ -1,40 +1,46 @@
+; Update: According to the website below,
+; the right-most column is 319 and
+; the bottom-most row is 199
+; instead of 320 and 200 respectively.
+; http://www.brackeen.com/vga/basics.html
+;
 ; Prevent drawing beyond boundaries.
     cmp     word [x0], 0                ; 0 <= x0 <= 200
     jge     is_x0_over_200
-    mov     word [x0], 0000h
+    mov     [x0], word 0
     jmp     is_x1_below_0
 is_x0_over_200:
     cmp     word [x0], 200d
     jle     is_x1_below_0
-    mov     word [x0], 200d
+    mov     [x0], word 200d
 
 is_x1_below_0:                          ; 0 <= x1 <= 200
     cmp     word [x1], 0
     jge     is_x1_over_200
-    mov     word [x1], 0000h
+    mov     [x1], word 0
     jmp     is_y0_below_0
 is_x1_over_200:
     cmp     word [x1], 200d
     jle     is_y0_below_0
-    mov     word [x1], 200d
+    mov     [x1], word 200d
 
 is_y0_below_0:                          ; 0 <= y0 <= 320
     cmp     word [y0], 0
     jge     is_y0_over_320
-    mov     word [y0], 0000h
+    mov     [y0], word 0
     jmp     is_y1_below_0
 is_y0_over_320:
     cmp     word [y0], 320d
     jle     is_y1_below_0
-    mov     word [y0], 320d
+    mov     [y0], word 320d
 
 is_y1_below_0:                          ; 0 <= y1 <= 320
     cmp     word [y1], 0
     jge     is_y1_over_320
-    mov     word [y1], 0000h
+    mov     [y1], word 0
 is_y1_over_320:
     cmp     word [y1], 320d
     jle     end_tests
-    mov     word [y1], 320d
+    mov     [y1], word 320d
 
 end_tests:
