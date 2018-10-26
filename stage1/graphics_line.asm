d77687c8b8d346863b2f726f261089531c84a9a9
Adam Morawiec <amrw+gitlab@pm.me>
2018-10-26 19:34:27 +0100

    Finish Stage 1
    - add main menu,
    - add few preset lines to the main menu,
    - add option for drawing multiple lines,
    - split setup call in two,
    - move Bresenham's line drawing algorithm into its own file,
    - move algorithm-specific variables into corresponding files.
    
    The program has ability to promt user to choose one of the few predefined lines. For single lines, the main menu goes into a colour choosing menu.
    
    The program can also draw multiple lines thanks to refactoring the line drawing algorithm into its own callable procedure.

--- stage1/graphics_line.asm
+++ stage1/graphics_line.asm
@@ -1,107 +1,13 @@
-; 8086 Assembly Bresenham’s line drawing algorithm.
-; Pseudo-code:
-;
-; function drawLine(x0, y0, x1, y1, colour)
-;   dx = abs(x1 - x0)
-;   dy = abs(y1 - y0)
-;   (x0 < x1) ? sx = 1 : sx = -1
-;   (y0 < y1) ? sx = 1 : sx = -1
-;   err = dx - dy
-;   loop
-;     printPixel(x0, y0, colour)
-;     if (x0 = x1) and (y0 = y1) exit loop
-;     e2 = 2 * err
-;     if (e2 > -dy) then
-;       err = err - dy
-;       x0 = x0 + sx
-;     end if
-;     if (e2 < dx) then
-;       err = err + dx
-;       y0 = y0 + sy
-;     end if
-;   end loop
+; Default coordinates can be set in graphics_line_main_menu.asm
 
 Graphics_Line:
-%include "graphics_line_menu.asm"
+%include "graphics_line_main_menu.asm"
+%include "graphics_line_colour_menu.asm"
 %include "graphics_line_test_boundaries.asm"
 %include "graphics_line_test_straight.asm"
 
-; Delta X
-    mov     ax, word [x1]
-    sub     ax, word [x0]
-
-; Instead of doing '(x0 < x1) ? sx=1 : sx=-1',
-; the program does '(x1 >= x0) ? sx=1 : sx=-1' by using the subtraction in place.
-    jge     Skip_Direction_X
-    mov     [sx], word -1d
-
-Skip_Direction_X:
-; cwd -- Convert word to doubleword, i.e. extend the sign bit of AX into DX.
-    cwd                                 ; abs(dx)
-    xor     ax, dx
-    sub     ax, dx
-
-    mov     [delta_x], ax               ; Store delta X
-
-;____________________
-; Delta Y
-    mov     ax, word [y1]
-    sub     ax, word [y0]
-    jge     Skip_Direction_Y            ; (y1 >= y0) ? sy=1 : sy=-1
-    mov     [sy], word -1d
-
-Skip_Direction_Y:
-    cwd                                 ; abs(dy)
-    xor     ax, dx
-    sub     ax, dx
-
-    mov     [delta_y], ax               ; Store delta Y
-
-;____________________
-; Err
-    mov     ax, word [delta_x]          ; err = dx - dy
-    sub     ax, word [delta_y]
-    mov     [err], ax
-
-;____________________
-;Graphics_Line_Main
-    call    Graphics_Setup
-
-Draw_Line_Loop_Repeat:
-    int     10h                         ; Print pixel
-
-    cmp     cx, word [x1]               ; if (x0==x1 && y0==y1) break;
-    jne     Loop_Continue
-    cmp     dx, word [y1]
-    je      Graphics_Done               ; break
-
-Loop_Continue:
-    mov     si, word [err]              ; e2 = 2 * err
-    add     si, word [err]
-    mov     [e2], si
-
-;Update_Row
-    mov     si, word [delta_y]          ; if (e2 > -dy)
-    neg     si
-    cmp     word [e2], si
-    jle     Update_Column
-
-    mov     si, word [err]              ; err -= dy
-    sub     si, word [delta_y]
-    mov     [err], si
-    add     cx, word [sx]               ; x0 += sx
-
-Update_Column:
-    mov     si, word [delta_x]
-    cmp     word [e2], si
-    jge     Draw_Line_Loop_Repeat
-
-    mov     si, word [err]              ; err += dx
-    add     si, word [delta_x]
-    mov     [err], si
-    add     dx, word [sy]               ; y0 += sy
-
-    jmp     Draw_Line_Loop_Repeat
+    call    Graphics_Set_Display_Mode
+    call    Bresenham_Main
 
 ;____________________
 Graphics_Done:
@@ -110,30 +16,30 @@ Graphics_Done:
 
     mov     ax, 0003h                   ; Return to text mode
     int     10h
-    jmp     Graphics_Line_Menu          ; Return to the Menu
+    jmp     Graphics_Line_Main_Menu     ; Return to the Menu
 
 ;____________________
-Graphics_Setup:
+Graphics_Set_Display_Mode:
     mov     ax, 0013h                   ; Set display mode to 320x200px, 256 colours, 1 page.
     int     10h
-    mov     ah, 0Ch                     ; Draw pixel instruction
+    ret
 
+Graphics_Setup:
     mov     cx, word [x0]               ; Column start
     mov     dx, word [y0]               ; Row start
     mov     al, byte [pixel_colour]
-
+    mov     ah, 0Ch                     ; Draw pixel instruction
     ret
+
+%include "graphics_line_bresenham.asm"
 
 ;____________________
 ; Data
-    x0: dw 160d                         ; Line start
-    y0: dw 197d
-    x1: dw 160d                         ; Line end
-    y1: dw 2d
+    x0: dw 0                            ; Line start
+    y0: dw 0
+    x1: dw 0                            ; Line end
+    y1: dw 0
 
-    sx: dw 1d                           ; Direction variables
-    sy: dw 1d
-    delta_x: dw 0
-    delta_y: dw 0
-    err: dw 0
-    e2: dw 0
+    pixel_colour: db 0
+
+graphics_line_menu_prompt_exit: db 'Press (ESC) to leave Line Drawing', 0
