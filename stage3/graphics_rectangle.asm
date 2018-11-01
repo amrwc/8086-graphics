37ede933d9a19b423956426fc0f09297cdcc109d
amrwc <42574496+amrwc@users.noreply.github.com>
2018-11-01 22:46:11 +0000

    Initial implementation of Stage 3
    - add rectangle drawing algorithm,
    - cover the case when the starting point is greater than the ending point,
    - create a simple menu,
    - clean up the code taken from previous stages,
    - prepare for drawing multiple rectangles simultaneously.
    
    Option 5 of the menu (draw all) is currently only plotting the last shape. It is most likely related to switching between video modes.

---
+++ stage3/graphics_rectangle.asm
@@ -0,0 +1,19 @@
+%include "graphics_rectangle_main.asm"
+%include "graphics_colour_menu.asm"
+%include "graphics_rectangle_algorithm.asm"
+
+Graphics_Rectangle:
+    push    150d   ; y1               ; Default arguments
+    push    190d   ; x1
+    push    50d    ; y0
+    push    50d    ; x0
+    call    Graphics_Rectangle_Main
+
+    jmp     Graphics_Rectangle
+
+;____________________
+Graphics_Rectangle_Setup:
+    mov     cx, word [bp + rect_x0]     ; Column start
+    mov     dx, word [bp + rect_y0]     ; Row start
+    mov     ax, word [bp + px_set]      ; AH: 0Ch, AL: colour
+    ret
