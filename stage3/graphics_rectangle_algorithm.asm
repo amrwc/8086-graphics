7baad271108e91574f8570fdc03930a1a3f2a4e5
amrwc <42574496+amrwc@users.noreply.github.com>
2018-11-03 14:34:21 +0000

    Add dimensions and boundaries tests, fix rectangle's height
    - add dimensions tests,
    - add boundaries tests,
    - fix rectangle algorithm to draw the last row,
    - move circle down one pixel in Vitruvian Man to touch the square's bottom edge.
    
    The new tests prevent drawing beyond boundaries. If the coordinates point outside of the screen, the tests will move the rectangle inside and draw the shape in full size -- if the space allows it.

--- stage3/graphics_rectangle_algorithm.asm
+++ stage3/graphics_rectangle_algorithm.asm
@@ -8,6 +8,9 @@
 %assign rect_row_end  2
 %assign rect_col_end  4
 
+%include "graphics_rectangle_test_dimensions.asm"
+%include "graphics_rectangle_test_boundaries.asm"
+
 Graphics_Rectangle_Algorithm:
     push    bp
     mov     bp, sp
@@ -16,7 +19,23 @@ Graphics_Rectangle_Algorithm:
     push    dx
     push    si
 
-    call    Graphics_Rectangle_Setup
+;____________________
+; Setup
+    call    Graphics_Rectangle_Test_Dimensions
+
+    mov     si, word [bp + rect_x0]     ; Column end
+    add     si, word [bp + rect_w]
+    mov     [bp - rect_row_end], si
+    mov     si, word [bp + rect_y0]     ; Row end
+    add     si, word [bp + rect_h]
+    mov     [bp - rect_col_end], si
+
+    call    Graphics_Rectangle_Test_Boundaries
+
+    mov     cx, word [bp + rect_x0]     ; Column start
+    mov     dx, word [bp + rect_y0]     ; Row start
+    mov     ax, word [bp + px_set]      ; AH: 0Ch, AL: colour
+
     jmp     Draw_Rectangle_Inner        ; Skip the first inc.
 
 Draw_Rectangle_Repeat:
@@ -29,7 +48,7 @@ Draw_Rectangle_Inner:
     mov     cx, [bp + rect_x0]          ; Get back to the start of the line.
     inc     dx
     cmp     dx, word [bp - rect_col_end]; if (y = h) break;
-    jne     Draw_Rectangle_Inner
+    jle     Draw_Rectangle_Inner
 
     pop     si
     pop     dx
