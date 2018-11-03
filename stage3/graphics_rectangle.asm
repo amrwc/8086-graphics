35fa1b535dcc22395b0b8422d30407a0a78a7359
amrwc <42574496+amrwc@users.noreply.github.com>
2018-11-03 15:26:29 +0000

    Remove redundant operand sizes, change rectangle's default width

--- stage3/graphics_rectangle.asm
+++ stage3/graphics_rectangle.asm
@@ -4,7 +4,7 @@
 Graphics_Rectangle:
     push    50d    ; x0                 ; Default arguments
     push    50d    ; y0
-    push    100d   ; width
+    push    140d   ; width
     push    100d   ; height
     push    0d     ; pixel settings placeholder
     call    Graphics_Rectangle_Main
